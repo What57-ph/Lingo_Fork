692425d131a53713c54f06775d50ab18621be908
Vin Com <0plmzxc@gmail.com>
2025-09-16 10:28:07 +0700

    merge slice

---
+++ googledriveservice/src/main/java/com/lingo/googledriveservice/GoogledriveserviceApplication.java
@@ -0,0 +1,13 @@
+package com.lingo.googledriveservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogledriveserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogledriveserviceApplication.class, args);
+	}
+
+}
---
+++ googledriveservice/src/test/java/com/lingo/googledriveservice/GoogledriveserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingo.googledriveservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogledriveserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
